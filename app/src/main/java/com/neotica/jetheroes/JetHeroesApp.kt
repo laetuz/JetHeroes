a80fbaa2bfc10c4a95ff023acdaec140d51b9199
Ryo Martin <laetuzg@gmail.com>
2023-05-10 22:59:09 +0700

    Added SearchBar.

--- app/src/main/java/com/neotica/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/neotica/jetheroes/JetHeroesApp.kt
@@ -4,24 +4,31 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
@@ -34,6 +41,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,18 +58,26 @@ fun JetHeroesApp(
     modifier: Modifier = Modifier,
     viewModel: HeroViewModel = getViewModel()
 ) {
+    val scope = rememberCoroutineScope()
+    val listState = rememberLazyListState()
+    val showButton: Boolean by remember {
+        derivedStateOf { listState.firstVisibleItemIndex > 0 }
+    }
+    val groupedHeroes by viewModel.groupedHeroes.collectAsState()
+    val query by viewModel.query
     Box(modifier = modifier) {
-        val scope = rememberCoroutineScope()
-        val listState = rememberLazyListState()
-        val showButton: Boolean by remember {
-            derivedStateOf { listState.firstVisibleItemIndex > 0 }
-        }
-        val groupedHeroes by viewModel.groupedHeroes.collectAsState()
         LazyColumn(
             state = listState
         ) {
+            item {
+                SearchBar(
+                    query = query,
+                    onQueryChange = viewModel::search,
+                    modifier = Modifier.background(MaterialTheme.colorScheme.primary)
+                )
+            }
             groupedHeroes.forEach { (initial, heroes) ->
-                stickyHeader { 
+                stickyHeader {
                     CharacterHeader(char = initial)
                 }
                 items(heroes, key = { it.id }) {
@@ -160,6 +176,36 @@ fun CharacterHeader(
                 .padding(8.dp)
         )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = query,
+        onValueChange = onQueryChange,
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        placeholder = { Text(stringResource(id = R.string.search_hero)) },
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp))
+    )
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/neotica/jetheroes/data/HeroViewModel.kt
+++ app/src/main/java/com/neotica/jetheroes/data/HeroViewModel.kt
@@ -1,5 +1,7 @@
 package com.neotica.jetheroes.data
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.neotica.jetheroes.model.Hero
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,4 +14,14 @@ class HeroViewModel(private val repository: HeroRepository) : ViewModel() {
             .groupBy { it.name[0] }
     )
     val groupedHeroes : StateFlow<Map<Char, List<Hero>>> get() = _groupedHeroes
+
+    private val _query = mutableStateOf("")
+    val query: State<String> get() = _query
+
+    fun search(newQuery: String) {
+        _query.value = newQuery
+        _groupedHeroes.value = repository.searchHeroes(_query.value)
+            .sortedBy { it.name }
+            .groupBy { it.name[0] }
+    }
 }
